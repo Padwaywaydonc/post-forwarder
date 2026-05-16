2e0de437b14f4c36abf970373fc8953638a1f64d
Sylvee <carla@MacBookPro.home>
2026-05-16 17:16:32 +0200

    claude: edit post-forwarder.php

--- post-forwarder.php
+++ post-forwarder.php
@@ -278,8 +278,6 @@ add_action('admin_init', function () {
                 $portals[ $key ] = array(
                     'type'              => 'meta',
                     'name'              => sanitize_text_field( $portal['name'] ),
-                    'app_id'            => sanitize_text_field( isset( $portal['app_id'] )     ? $portal['app_id']     : '' ),
-                    'app_secret'        => sanitize_text_field( isset( $portal['app_secret'] ) ? $portal['app_secret'] : '' ),
                     'post_to_facebook'  => ! empty( $portal['post_to_facebook'] ),
                     'post_to_instagram' => ! empty( $portal['post_to_instagram'] ),
                 );
