090fab32eb839140ae3ee5676ba8daf58a7dba3c
Sylvee <carla@MacBookPro.home>
2026-05-16 17:16:48 +0200

    claude: edit post-forwarder.php

--- post-forwarder.php
+++ post-forwarder.php
@@ -386,23 +386,17 @@ add_action('admin_init', function () {
         }
     }
 
-    // Save & Connect — Meta (Facebook + Instagram).
+    // Save & Connect — Meta (Facebook + Instagram) via relay.
     if ( ! empty( $_POST['pending_meta_connect'] ) ) {
         $meta_connect_key = sanitize_key( wp_unslash( $_POST['pending_meta_connect'] ) );
-        if ( isset( $portals[ $meta_connect_key ] ) && 'meta' === $portals[ $meta_connect_key ]['type'] ) {
-            $app_id     = $portals[ $meta_connect_key ]['app_id'];
-            $app_secret = $portals[ $meta_connect_key ]['app_secret'];
-            if ( $app_id && $app_secret ) {
-                $nonce        = wp_create_nonce( 'meta_auth_' . $meta_connect_key );
-                $callback_url = admin_url( 'options-general.php?page=post-forwarding&meta_oauth_callback=1' );
-                wp_redirect( 'https://www.facebook.com/v21.0/dialog/oauth?' . http_build_query( array(
-                    'client_id'    => $app_id,
-                    'redirect_uri' => $callback_url,
-                    'scope'        => 'pages_show_list,pages_read_engagement,pages_manage_posts,instagram_basic,instagram_content_publish',
-                    'state'        => $meta_connect_key . '|' . $nonce,
-                ) ) );
-                exit;
-            }
+        $meta_relay       = post_forwarder_relay_url();
+        if ( $meta_relay && isset( $portals[ $meta_connect_key ] ) && 'meta' === $portals[ $meta_connect_key ]['type'] ) {
+            wp_redirect( $meta_relay . '/meta/start?' . http_build_query( array(
+                'return_url' => $settings_url,
+                'portal_key' => $meta_connect_key,
+                'wp_nonce'   => wp_create_nonce( 'meta_oauth_' . $meta_connect_key ),
+            ) ) );
+            exit;
         }
     }
 
