49ecd7d4dc883a10a80fe20929722ecf48023282
Sylvee <carla@MacBookPro.home>
2026-05-16 17:18:15 +0200

    claude: edit post-forwarder.php

--- post-forwarder.php
+++ post-forwarder.php
@@ -1838,8 +1838,6 @@ function post_forwarding_settings_page() {
                     ? '<button type="button" class="button save-and-connect-x" style="background:#000;border-color:#000;color:#fff;">&#10132; <?php echo esc_js( __( 'Save &amp; Connect with X', 'post-forwarder' ) ); ?></button>'
                     : '<span style="color:#666;"><?php echo esc_js( __( 'X connection requires the relay. Configure POST_FORWARDER_RELAY_URL first.', 'post-forwarder' ) ); ?></span>'
                 ) + '</td></tr>' +
-                '<tr class="fields-meta" style="display:none;"><th><?php echo esc_js( __( 'App ID', 'post-forwarder' ) ); ?></th><td><input type="text" name="portals[' + n + '][app_id]" placeholder="<?php echo esc_js( __( 'Meta App ID', 'post-forwarder' ) ); ?>" style="width:240px;" /></td></tr>' +
-                '<tr class="fields-meta" style="display:none;"><th><?php echo esc_js( __( 'App Secret', 'post-forwarder' ) ); ?></th><td><input type="text" name="portals[' + n + '][app_secret]" placeholder="<?php echo esc_js( __( 'Meta App Secret', 'post-forwarder' ) ); ?>" style="width:300px;" /></td></tr>' +
                 '<tr class="fields-meta" style="display:none;"><th><?php echo esc_js( __( 'Post to', 'post-forwarder' ) ); ?></th><td>' +
                 '<label><input type="checkbox" name="portals[' + n + '][post_to_facebook]" value="1" checked> <?php echo esc_js( __( 'Facebook Page', 'post-forwarder' ) ); ?></label>&nbsp;&nbsp;' +
                 '<label><input type="checkbox" name="portals[' + n + '][post_to_instagram]" value="1" checked> <?php echo esc_js( __( 'Instagram', 'post-forwarder' ) ); ?></label>' +
