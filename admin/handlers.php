9dd6b5baa36b02b97109464ea5438fd1c2e498e8
Sylvee <carla@MacBookPro.home>
2026-05-24 14:28:12 +0200

    claude: edit admin/handlers.php

--- admin/handlers.php
+++ admin/handlers.php
@@ -592,7 +592,7 @@ function post_forwarder_ajax_add_channel() {
         wp_send_json_error( array( 'message' => __( 'Channel name is required.', 'post-forwarder' ) ) );
     }
 
-    $allowed_types = array( 'linkedin', 'x', 'wordpress' );
+    $allowed_types = array( 'linkedin', 'x', 'meta', 'wordpress' );
     if ( ! in_array( $type, $allowed_types, true ) ) {
         $type = 'wordpress';
     }
