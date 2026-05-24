a50648aa7457dbd4a94e012e97cc18e4687522c5
Sylvee <carla@MacBookPro.home>
2026-05-24 14:27:19 +0200

    claude: edit includes/helpers.php

--- includes/helpers.php
+++ includes/helpers.php
@@ -57,6 +57,8 @@ function post_forwarder_channel_connected( array $m ) {
             return (bool) post_forwarder_relay_url()
                 && ! empty( $m['access_token'] )
                 && ( empty( $m['token_expires'] ) || $m['token_expires'] > time() );
+        case 'meta':
+            return ! empty( $m['access_token'] ) && ! empty( $m['page_id'] );
         case 'wordpress':
             return ! empty( $m['user'] ) && ! empty( $m['password'] );
         default:
