b93ff7eb3e8bccbda2e1f34ab4c9a8f08552318f
Sylvee <carla@MacBookPro.home>
2026-06-16 22:01:45 +0200

    claude: edit admin/settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -970,7 +970,6 @@ function post_forwarder_settings_page() {
             $row.find('.fields-wordpress').toggle(type === 'wordpress');
             $row.find('.fields-linkedin').toggle(type === 'linkedin');
             $row.find('.fields-x').toggle(type === 'x');
-            $row.find('.fields-meta').toggle(type === 'meta');
             $row.find('.test-connection').toggle(type === 'wordpress');
         }
 
