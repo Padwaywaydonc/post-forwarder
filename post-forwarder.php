6914944a7d5b91ecd7d3258c6636df1b149138c4
Sylvee <carla@MacBookPro.home>
2026-06-16 23:55:52 +0200

    claude: edit post-forwarder.php

--- post-forwarder.php
+++ post-forwarder.php
@@ -10,7 +10,7 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: post-forwarder
  * Requires at least: 5.0
- * Tested up to: 6.8
+ * Tested up to: 7.0
  * Requires PHP: 7.4
  */
 
